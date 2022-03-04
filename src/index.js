913c64f12b68937acedd628b3d941cad6de0867f
jbiers <juliabiersuriano@gmail.com>
2022-03-04 11:39:38 -0400

    p5.js setup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
-const div = document.createElement('div');
-div.innerHTML = "hello world";
+import * as p5 from 'p5';
 
-document.body.appendChild(div);
+let s = (sk) => {
+    sk.setup = () => {
+        let gfx = sk.createGraphics(window.innerWidth, window.innerHeight);
+        let gfx2;
+        sk.createCanvas(window.innerWidth, window.innerHeight)
+        sk.background(40);
+        gfx.stroke(200);
+        gfx.strokeWeight(3);
+
+        for (let i = 0; i < 1000; i++) {
+            gfx.point(
+                Math.random()
+                * window.innerWidth, Math.random()
+            * window.innerHeight
+            );
+        }
+        //creating the cloned object
+        gfx2 = { ...gfx };
+        sk.image(gfx, 0, 0);
+        sk.image(gfx2, 2, 2);
+    }
+
+    sk.draw = () => {
+
+    }
+}
+
+const P5 = new p5(s);
